771196120285bcd2d2608e7dbf47b318e9c0abac
Adam Moelis <adam.moelis@gmail.com>
2015-09-18 17:37:56 -0400

    add login, sign up, logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :users
   root 'posts#index'
   resources :posts
+  delete '/logout', to: "sessions#destroy", as: "logout"
+  get '/login', to: "sessions#new", as: "login"
+  post '/login', to: "sessions#create"
   delete 'posts/:id', to: 'posts#destroy', as: 'destroy_post'
+
 end
