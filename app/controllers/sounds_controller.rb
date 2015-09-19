30655090bd0e3fb644a9ae83f7ec7deb83e31c19
EJLarcs <ejlarcombe@gmail.com>
2015-09-19 09:19:39 -0400

    Refactoring the gemfile

---
+++ app/controllers/sounds_controller.rb
@@ -0,0 +1,21 @@
+require 'soundcloud'
+require 'open-uri'
+require 'rubygems'
+require 'json'
+
+class SoundsController < ApplicationController
+
+  # def show
+  #   client_playlist = Soundcloud.new(:client_id => Rails.application.secrets.soundcloud_client_id,
+  #                       :client_secret => Rails.application.secrets.soundcloud_secret,
+  #                       :username => Rails.application.secrets.soundcloud_username,
+  #                       :password => Rails.application.secrets.soundcloud_password)
+  #   client_track = SoundCloud.new(:client_id => Rails.application.secrets.soundcloud_client_id)
+  #   track_url_array = Post.find(params[:id]).try(:track_url)
+  #   embed_info = client_track.get('/oembed', :url => track_url_array)
+  #   @song = embed_info['html']
+  #   @track_id = client_track.get('/resolve', :url => track_url_array).id
+  #   @playlists = client_playlist.get("/me/playlists")
+  # end
+
+end
