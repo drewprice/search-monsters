53f4506175413db9cbbf8411059049cf9ad4ea8b
Drew Price <drewprice@users.noreply.github.com>
2015-09-22 18:09:53 -0400

    Update searchbox_url

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['http://paas:f5f81a72ebc67cc8e7ca9d0d50171291@fili-us-east-1.searchly.com']
+Elasticsearch::Model.client = Elasticsearch::Client.new host: 'http://paas:b43b410f61b8fa79fe01a8cddb2136c6@fili-us-east-1.searchly.com'
 
 module CapsuleMonsters
   class Application < Rails::Application
