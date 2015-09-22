18bc24dd747478c3c33ad04ab3be679df00709e2
jessicashannon <jessdahling@gmail.com>
2015-09-22 14:20:30 -0400

    add faker, move logic from controller to model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user           = User.new(user_params)
-    @user.username  = User.random_name
-    @user.image_src = User.random_src
-
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to posts_path
