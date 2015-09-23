75dfdb9aa45b0efa38ab6158c03a73747e41cc83
Drew Price <drewprice@users.noreply.github.com>
2015-09-23 11:23:52 -0400

    Revert "Janey branch"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to posts_path
     else
-      flash[:notice] = @user.errors.messages
-      redirect_to root_path
+      render 'sessions/index', alert: 'Something went wrong... try again!'
     end
   end
 
