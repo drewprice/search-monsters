23953da550eff7a7bf46eb67967955800e83de93
Drew Price <drewprice@users.noreply.github.com>
2015-09-23 22:17:31 -0400

    Cleanup & just a touch of refactoring

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ActiveRecord::Base
+  POSTS_PER_PAGE = 20
+
   belongs_to :user
 
   validates :body, presence: true
 
-  def self.num_per_page
-    20
+  def self.all_for(page)
+    reorder("created_at DESC").page(page).per_page(POSTS_PER_PAGE)
   end
 end
