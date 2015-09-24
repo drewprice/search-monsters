1ded32c99d0969206ad009a5ef9d318f6bd4a257
jessicashannon <jessdahling@gmail.com>
2015-09-24 13:44:03 -0400

    user friendly flash errors with sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:user][:email])
-
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to posts_path
     else
-      redirect_to root_path, alert: 'Something was not correct. Do it again, yo.'
+      flash[:notice] = 'Your email or password was incorrect. Please try again.'
+      redirect_to root_path
     end
   end
 
